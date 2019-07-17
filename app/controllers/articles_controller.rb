6108cb0826c6d6264fcb75beadf2dd9d123d542c
--globally <tyagimanish1@gmail.com>
2019-07-17 13:20:40 +0530

    CRUD

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,38 @@
+class ArticlesController < ApplicationController
+	def new
+	end
+	def create
+		
+  		#render plain: params[:article].inspect
+  		#@article = Article.new(params[:article])
+
+		@article = Article.new(params.require(:article).permit(:title, :text))
+ 
+  		@article.save
+  		redirect_to @article
+
+  	end
+  	def show
+  		 @article = Article.find(params[:id])
+	end
+	def index
+    	@articles = Article.all
+  	end
+  	private
+		def article_params
+    	params.require(:article).permit(:title, :text)
+  	end
+  	def edit
+  		@article = Article.find(params[:id])
+	end	
+	def update
+  @article = Article.find(params[:id])
+ 
+  if @article.update(article_params)
+    redirect_to @article
+  else
+    render 'edit'
+  end
+end
+end
+
