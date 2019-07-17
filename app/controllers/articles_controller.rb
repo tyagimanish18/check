a05cfb19d6689fdbd99c7735576c96c62f0b2c1f
--globally <tyagimanish1@gmail.com>
2019-07-17 15:38:42 +0530

    CRUD COMPLETE

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,38 +1,45 @@
 class ArticlesController < ApplicationController
 	def new
 	end
+
+  #render plain: params[:article].inspect
+  #@article = Article.new(params[:article])
 	def create
-		
-  		#render plain: params[:article].inspect
-  		#@article = Article.new(params[:article])
-
 		@article = Article.new(params.require(:article).permit(:title, :text))
- 
-  		@article.save
-  		redirect_to @article
+  	@article.save
+  	redirect_to @article
+  end
 
-  	end
-  	def show
-  		 @article = Article.find(params[:id])
+  def show
+  	@article = Article.find(params[:id])
 	end
+
 	def index
     	@articles = Article.all
-  	end
-  	private
-		def article_params
-    	params.require(:article).permit(:title, :text)
   	end
   	def edit
   		@article = Article.find(params[:id])
 	end	
 	def update
-  @article = Article.find(params[:id])
+  		@article = Article.find(params[:id])
  
-  if @article.update(article_params)
-    redirect_to @article
-  else
-    render 'edit'
+	    if @article.update(article_params)
+    		redirect_to @article
+  		else
+    		render 'edit'
+		end
+	end
+
+	
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+ 
+    redirect_to articles_path
   end
-end
+  	private
+		def article_params
+    	params.require(:article).permit(:title, :text)
+  	end
 end
 
