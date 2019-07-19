54d1735ea20f52df82027b122a56dd00187cc4ae
--globally <tyagimanish1@gmail.com>
2019-07-19 17:20:21 +0530

    added comments on blog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+    http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
 	def new
 	end
 
@@ -6,20 +7,22 @@ class ArticlesController < ApplicationController
   #@article = Article.new(params[:article])
 	def create
 		@article = Article.new(params.require(:article).permit(:title, :text))
-  	@article.save
+    	@article.save
   	redirect_to @article
   end
 
   def show
-  	@article = Article.find(params[:id])
+  	@article = Article.find(params[:id])                  
 	end
 
 	def index
-    	@articles = Article.all
-  	end
+    @articles = Article.all
+  end
+
   	def edit
   		@article = Article.find(params[:id])
 	end	
+
 	def update
   		@article = Article.find(params[:id])
  
@@ -30,16 +33,16 @@ class ArticlesController < ApplicationController
 		end
 	end
 
-	
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
  
     redirect_to articles_path
   end
-  	private
-		def article_params
-    	params.require(:article).permit(:title, :text)
-  	end
+
+	private
+	def article_params
+  	params.require(:article).permit(:title, :text)
+	end
 end
 
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
+    #ssssshas_many :comments
+    has_many :comments, dependent: :destroy
 	  validates :title, presence: true,
                     length: { minimum: 5 }
+
 end
